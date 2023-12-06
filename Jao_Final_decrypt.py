806e54462b4242e3f40372944927052cd6280b23
jaosherlyr <jaosherlyr@gmail.com>
2023-12-07 01:46:33 +0800

    Initial commit

---
+++ Jao_Final_decrypt.py
@@ -0,0 +1,193 @@
+#  ====================
+#  Sherly R. Jao
+#  BSCS - 3
+#  Final Output of CS 3101N
+#  ====================
+
+import os
+import sys
+import hashlib as hl
+import json
+
+#  ===== INPUT =====
+print("\n> Accept Filename")
+filename = input("Enter name of text file: ")
+
+#  Check if the file exist
+if os.path.exists(filename):
+    with open(filename, 'r') as file:
+        content = file.read()
+        ciphertext = [int(item.strip()) for item in content.split(', ')]
+else:
+    print(f"\n!! File: '{filename}' does not exist. Make sure your filename is correct. Exiting....")
+    sys.exit(1)
+
+#  Accept user input keys
+print("\n> Accept Keys")
+
+#  Check if the file exist
+while True:
+    keys_filename = input("Enter name of text file containing the generated keys: ")
+
+    if not os.path.exists(keys_filename):
+        print(f"\n!! File: '{keys_filename}' does not exist. Make sure your filename is correct. Try agian.")
+    else:
+        with open(keys_filename, 'r') as file:
+            key_contents = json.load(file)
+        break
+
+#  distribute key contents
+otp = key_contents["otp"]
+decryption_key = key_contents["decryption_key"]
+loop_num = key_contents["loop_num"]
+
+while True:
+    caesar_key = input("Enter Caesar Cipher Key: ")
+    #  check if key is a digit
+    if not caesar_key.isdigit():
+        print("\n!! Caesar Cipher Key MUST be a digit. Enter new key.\n")
+    else:
+        caesar_key = int(caesar_key)
+        break
+
+while True:
+    transposition_key = input("Enter Transposition Cipher Key: ")
+    #  check if the transposition key has same letters
+    if any(transposition_key.count(letter) > 1 for letter in transposition_key):
+        print("\n!! Transposition Cipher Key must NOT have duplicate letters. Enter new key.\n")
+    else:
+        break
+
+vigenere_key = input("Enter Vigenère Cipher Key: ")
+
+
+#  ===== LOGIC =====
+def rsa_decrypt(text, key):
+    d, n = key
+    decrypted_text = [chr(pow(char, d, n)) for char in text]
+    return ''.join(decrypted_text)
+
+
+def vernam_decrypt(text, key):
+    decrypted_text = ''.join(chr(ord(c) ^ ord(k)) for c, k in zip(text, key))
+    return decrypted_text
+
+
+def vigenere_decrypt(text, key):
+    decrypted_text = ""
+    len_key = len(key)
+
+    for i, char in enumerate(text):
+        if char.isalpha():
+            is_upper = char.isupper()
+            key_char = key[i % len_key].upper()
+            shifted_char = chr((ord(char) - ord(key_char) + 26) % 26 + ord('A' if is_upper else 'a'))
+            decrypted_text += shifted_char
+        else:
+            #  retain char if not alphabet
+            decrypted_text += char
+
+    return decrypted_text
+
+
+def caesar_encrypt(text, key):
+    ciphertext = ''
+
+    for char in text:
+        if char.isalpha():
+            is_upper = char.isupper()
+            shifted_char = chr((ord(char) - ord('A' if is_upper else 'a') + key) % 26 + ord('A' if is_upper else 'a'))
+            ciphertext += shifted_char
+        else:
+            #  retain char if not alphabet
+            ciphertext += char
+
+    return ciphertext
+
+
+def caesar_decrypt(text, key):
+    return caesar_encrypt(text, -key)
+
+
+def transposition_decrypt(text, key):
+    sorted_key = ''.join(sorted(key))
+
+    num_columns = len(sorted_key)
+    num_rows = (len(text) + num_columns - 1) // num_columns
+    grid = [[' ' for _ in range(num_columns)] for _ in range(num_rows)]
+    char_index = 0
+
+    for col_char in sorted_key:
+        col_index = key.index(col_char)
+        for row in range(num_rows):
+            grid[row][col_index] = text[char_index]
+            char_index += 1
+
+    decrypted_key = ''.join(''.join(grid[row]) for row in range(num_rows))
+
+    return decrypted_key
+
+
+#  ===== DECRYPTION =====
+rsa_decrypted_text = rsa_decrypt(ciphertext, decryption_key)
+vernam_decrypted_text = vernam_decrypt(rsa_decrypted_text, otp)
+vigenere_decrypted_text = vigenere_decrypt(vernam_decrypted_text, vigenere_key)
+caesar_decrypted_text = caesar_decrypt(vigenere_decrypted_text, caesar_key)
+text = caesar_decrypted_text
+
+for i in range(loop_num):
+    transposition_decrypted_text = transposition_decrypt(text, transposition_key)
+    text = transposition_decrypted_text
+
+decrypted_text = transposition_decrypted_text.strip()
+
+#  ===== OUTPUT ======
+def decrypted_text_to_file(filename, content):
+    filename = filename[len("encrypted_"):]
+    name = f"decrypted_{filename}"
+    with open(name, 'w') as file:
+        file.write(content)
+
+
+decrypted_text_to_file(filename, decrypted_text)
+
+#  ===== HASH CHECKING =====
+print("\n> Hash Checking")
+
+while True:
+    original_file = input("Enter original file for has comparison: ")
+
+    if not os.path.exists(original_file):
+        print(f"\n!! File: '{original_file}' does not exist. Make sure your filename is correct. Try agian.")
+    else:
+        break
+
+
+def get_hash(content):
+    binary_data = content.encode("utf-8")
+    md5 = hl.md5(binary_data).hexdigest()
+    sha1 = hl.sha1(binary_data).hexdigest()
+
+    return binary_data, md5, sha1
+
+
+with open(original_file, 'r') as file:
+    content = file.read()
+
+original_binary_data, original_md5, original_sha1 = get_hash(content)
+decrypted_binary_data, decrypted_md5, decrypted_sha1 = get_hash(decrypted_text)
+
+are_identical = original_binary_data == decrypted_binary_data and original_md5 == decrypted_md5 and original_sha1 == decrypted_sha1
+
+if are_identical:
+    print("\n\n!! DECRYPTION SUCCESS !!")
+    print(f"\nOriginal File: {original_file}")
+    print(f"\tBinary Data: {original_binary_data}")
+    print(f"\tMD5: {original_md5}")
+    print(f"\tSHA1 Data: {original_sha1}")
+    print(f"\nDecrypted File: decrypted_{original_file}")
+    print(f"\tBinary Data: {decrypted_binary_data}")
+    print(f"\tMD5: {decrypted_md5}")
+    print(f"\tSHA1 Data: {decrypted_sha1}")
+else:
+    print("\n!! DECRYPTION UNSUCCESSFUL. There was a problem in your decryption !!")
---
+++ Jao_Final_encrypt.py
@@ -0,0 +1,222 @@
+#  ====================
+#  Sherly R. Jao
+#  BSCS - 3
+#  Final Output of CS 3101N
+#  ====================
+
+import os
+import sys
+import random
+import math
+import json
+
+#  ===== INPUT =====
+print("\n> Accept Filename")
+filename = input("Enter name of text file: ")
+
+#  Check if the file exist
+if os.path.exists(filename):
+    with open(filename, 'r') as file:
+        plaintext = file.read()
+else:
+    print(f"\n!! File: '{filename}' does not exist. Make sure your filename is correct. Exiting....")
+    sys.exit(1)
+
+print("\n> Accept Keys")
+
+while True:
+    caesar_key = input("Enter Caesar Cipher Key: ")
+    if not caesar_key.isdigit():
+        print("\n!! Caesar Cipher Key MUST be a digit. Enter new key.\n")
+    else:
+        caesar_key = int(caesar_key)
+        break
+
+while True:
+    transposition_key = input("Enter Transposition Cipher Key: ")
+    #  check if the transposition key has same letters
+    if any(transposition_key.count(letter) > 1 for letter in transposition_key):
+        print("\n!! Transposition Cipher Key must NOT have duplicate letters. Enter new key.\n")
+    else:
+        break
+
+vigenere_key = input("Enter Vigenère Cipher Key: ")
+
+
+#  ===== GENERATE RSA KEYS =====
+def is_prime(num):
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if (num % i) == 0:
+            return False
+    return True
+
+
+def get_prime(bits):
+    while True:
+        p_num = random.getrandbits(bits)
+        if is_prime(p_num):
+            return p_num
+
+
+def extended_gcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        g, x, y = extended_gcd(b % a, a)
+        return g, y - (b // a) * x, x
+
+
+def modular_inverse(integer, modulo):
+    g, x, _ = extended_gcd(integer, modulo)
+    if g != 1:
+        raise Exception('Modular inverse does not exist')
+    else:
+        return x % modulo
+
+
+def generate_rsa_keys(bits):
+    p = get_prime(bits)
+
+    while True:
+        q = get_prime(bits)
+        if p != q:
+            break
+
+    n = p * q
+    phi = (p - 1) * (q - 1)
+
+    while True:
+        #  generate am e that is greater than 1 and less than phi
+        e = random.randint(2, phi - 1)
+        if math.gcd(e, phi) == 1:
+            break
+
+    #  generate d which is the modular inverse of e
+    d = modular_inverse(e, phi)
+
+    encrypt_key = (e, n)
+    decrypt_key = (d, n)
+
+    return encrypt_key, decrypt_key
+
+
+#  ===== LOGIC =====
+def transposition_encrypt(text, key):
+    sorted_key = ''.join(sorted(key))
+
+    num_columns = len(sorted_key)
+    num_rows = (len(text) + (num_columns - 1)) // num_columns
+    grid = [[' ' for _ in range(num_columns)] for _ in range(num_rows)]
+
+    for i, char in enumerate(text):
+        row = i // num_columns
+        columns = i % num_columns
+        grid[row][columns] = char
+
+    ciphertext = ''
+    for char_column in sorted_key:
+        index = key.index(char_column)
+        column = [grid[row][index] for row in range(num_rows)]
+        ciphertext += ''.join(column)
+
+    return ciphertext
+
+
+def get_loop(num):
+    factor = num
+
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            factor = i
+
+    #  return 5 if the factor is greater than 5 otherwise return the smallest
+    return factor if factor < 5 else 5
+
+
+def caesar_encrypt(text, key):
+    ciphertext = ''
+
+    for char in text:
+        if char.isalpha():
+            is_upper = char.isupper()
+            shifted_char = chr((ord(char) - ord('A' if is_upper else 'a') + key) % 26 + ord('A' if is_upper else 'a'))
+            ciphertext += shifted_char
+        else:
+            #  retain char if not alphabet
+            ciphertext += char
+
+    return ciphertext
+
+
+def vigenere_encrypt(text, key):
+    ciphertext = ''
+    len_key = len(key)
+
+    for i, char in enumerate(text):
+        if char.isalpha():
+            is_upper = char.isupper()
+            key_char = key[i % len_key].upper()
+            shifted_char = chr((ord(char) + ord(key_char) - 2 * ord('A' if is_upper else 'a')) % 26 + ord('A' if is_upper else 'a'))
+            ciphertext += shifted_char
+        else:
+            #  retain char if not alphabet
+            ciphertext += char
+
+    return ciphertext
+
+
+def get_otp(len_text):
+    return ''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ') for _ in range(len_text))
+
+
+def vernam_encrypt(text, key):
+    ciphertext = ''.join(chr(ord(p) ^ ord(k)) for p, k in zip(text, key))
+    return ciphertext
+
+
+bits = 8
+encryption_key, decryption_key = generate_rsa_keys(bits)
+
+
+def rsa_encrypt(text, key):
+    e, n = key
+    ciphertext = [pow(ord(char), e, n) for char in text]
+    return ciphertext
+
+
+#  ==== ENCRYPTION =====
+transposition_ciphertext = plaintext
+loop_num = get_loop(len(plaintext))
+
+for i in range(loop_num):
+    transposition_ciphertext = transposition_encrypt(transposition_ciphertext, transposition_key)
+
+caesar_ciphertext = caesar_encrypt(transposition_ciphertext, caesar_key)
+vigenere_ciphertext = vigenere_encrypt(caesar_ciphertext, vigenere_key)
+otp = get_otp(len(vigenere_ciphertext))
+vernam_ciphertext = vernam_encrypt(vigenere_ciphertext, otp)
+rsa_ciphertext = rsa_encrypt(vernam_ciphertext, encryption_key)
+
+#  ===== OUTPUT =====
+def encrypted_text_to_file(filename, content):
+    name = f"encrypted_{filename}"
+    with open(name, 'w') as file:
+        file.write(', '.join(str(item) for item in content))
+
+
+def keys_to_file(filename, otp, decryption_key, loop_num):
+    content = {
+        "otp": otp,
+        "decryption_key": decryption_key,
+        "loop_num": loop_num
+    }
+
+    name = f"keys_{filename}"
+    with open(name, 'w') as file:
+        json.dump(content, file)
+
+
+encrypted_text_to_file(filename, rsa_ciphertext)
+keys_to_file(filename, otp, decryption_key, loop_num)
+
+print("\n!! ENCRYPTION SUCCESS !!\n")
